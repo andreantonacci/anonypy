b1475ffbb722c18d2504cee8f09e32307ee48fec
Andrea Antonacci <antonacciad@gmail.com>
2019-06-30 22:46:04 +0200

    Add pepper

--- anonypy.py
+++ anonypy.py
@@ -6,12 +6,12 @@ from Cryptodome.Hash import SHA256
 def anonymize(*args):
     # Get the input value from text_entry
     string = text_entry.get()
-    # Retrieve hidden salt
-    f = open('salt.txt', 'r', encoding='utf-8')
-    salt = f.readlines()
+    # Retrieve hidden pepper
+    f = open('pepper.txt', 'r', encoding='utf-8')
+    pepper = f.readlines()
     hash_obj = SHA256.new(data=string.encode('utf-8'))
-    # Update hash adding salt
-    hash_obj.update(salt[0].encode('utf-8'))
+    # Update hash adding pepper
+    hash_obj.update(pepper[0].encode('utf-8'))
     # Set output to digest
     digest.set(hash_obj.hexdigest())
     f.close()
